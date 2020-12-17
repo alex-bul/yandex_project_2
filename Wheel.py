0a68748564b7f66f95d40f48a06cc7d54c7a8b6e
DeDUnitY <gtanegta4@gmail.com>
2020-12-18 00:40:09 +0200

    2

--- Wheel.py
+++ Wheel.py
@@ -9,6 +9,7 @@ pygame.display.set_caption('Wheel')
 fps = 60
 deg = 0
 bet = 100
+summ = 100
 chose = ''
 data = eval(open('user data.txt', 'r').read())
 coin = data[-1]
@@ -29,6 +30,17 @@ def load_image(name, color_key=None):
     else:
         image = image.convert_alpha()
     return image
+
+
+def results():
+    global coin, chose, deg
+    if ((0 <= deg <= 25.5) or (77 <= deg <= 128.5) or (180 <= deg <= 231.5) or (
+            334.5 <= deg <= 360)) and chose == 'Black':
+        coin += bet * 2
+    elif (283 <= deg <= 334.5) and chose == 'Green':
+        coin += bet * 5
+    elif ((25.5 <= deg <= 77) or (128.5 <= deg <= 180) or (231.5 <= deg <= 283)) and chose == 'Red':
+        coin += bet * 2
 
 
 def draw(screen, view, x, y, cent, size=50):
@@ -84,6 +96,30 @@ green.image = green_image
 green.rect = green.image.get_rect()
 green.rect.center = red.rect.center[0] - 90, red.rect.center[1]
 
+add_bet_image = load_image('+.png')
+add_bet = pygame.sprite.Sprite(all_sprites)
+add_bet.image = add_bet_image
+add_bet.rect = add_bet.image.get_rect()
+add_bet.rect.center = red.rect.center[0] - 90, red.rect.center[1] + 100
+
+deduct_bet_image = load_image('-.png')
+deduct_bet = pygame.sprite.Sprite(all_sprites)
+deduct_bet.image = deduct_bet_image
+deduct_bet.rect = deduct_bet.image.get_rect()
+deduct_bet.rect.center = add_bet.rect.center[0] + 90, add_bet.rect.center[1]
+
+add_sum_image = load_image('+.png')
+add_sum = pygame.sprite.Sprite(all_sprites)
+add_sum.image = add_sum_image
+add_sum.rect = add_sum.image.get_rect()
+add_sum.rect.center = add_bet.rect.center[0], add_bet.rect.center[1] + 130
+
+deduct_sum_image = load_image('-.png')
+deduct_sum = pygame.sprite.Sprite(all_sprites)
+deduct_sum.image = deduct_sum_image
+deduct_sum.rect = deduct_sum.image.get_rect()
+deduct_sum.rect.center = add_sum.rect.center[0] + 90, add_sum.rect.center[1]
+
 hero_image = load_image('wheel4.png')
 hero = pygame.sprite.Sprite(all_sprites)
 hero.image = hero_image
@@ -108,7 +144,7 @@ while running:
         key = pygame.key.get_pressed()
         if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and not spin:
             mous_x, mous_y = pygame.mouse.get_pos()
-            for i in [new_deg, black, red, green]:
+            for i in [new_deg, black, red, green, add_bet, deduct_bet, add_sum, deduct_sum]:
                 if (mous_x in range(list(i.rect)[0], list(i.rect)[0] + list(i.rect)[2])) and (
                         mous_y in range(list(i.rect)[1], list(i.rect)[1] + list(i.rect)[3])):
                     centre = i.rect.center
@@ -123,6 +159,14 @@ while running:
                         chose = 'Red'
                     elif centre == (860, 400):
                         chose = 'Green'
+                    elif centre == (860, 500):
+                        bet += summ
+                    elif centre == (950, 500) and summ < bet:
+                        bet -= summ
+                    elif centre == (860, 630):
+                        summ += 10
+                    elif centre == (950, 630) and summ >= 10:
+                        summ -= 10
 
         if key[pygame.K_LEFT]:
             speed -= 1
@@ -130,10 +174,12 @@ while running:
             speed += 1
         if key[pygame.K_SPACE]:
             speed = 0
-        if event.type == pygame.MOUSEBUTTONDOWN and (event.button in [4, 5]) and not spin:
+        if event.type == pygame.MOUSEBUTTONDOWN and (
+                event.button in [4, 5]) and not spin and chose != '' and bet <= coin:
             deg = 0
             cof = random.choice([-1, 1])
             speed = random.randint(10, 25) * cof
+            coin -= bet
             spin = True
     if spin:
         if (speed >= 0 and cof == 1) or (speed <= 0 and cof == -1):
@@ -144,6 +190,7 @@ while running:
             else:
                 speed -= 0.09 * cof
             if speed <= 0 and cof == 1 or speed >= 0 and cof == -1:
+                results()
                 spin = False
                 print(deg)
             hero.image, hero.rect, deg = rot_center(defhero.image, hero.rect, (deg - speed) % 360)
@@ -153,6 +200,7 @@ while running:
     clock.tick(fps)
     draw(screen, view=f'You Chose: {chose}', x=825, y=290, cent=False, size=40)
     draw(screen, view=f'You Bet: {bet}', x=825, y=325, cent=False, size=40)
+    draw(screen, view=f'Sum to change: {summ}', x=825, y=550, cent=False, size=40)
     draw(screen, view=f'You Coin: {coin}', x=width // 2, y=30, cent=True, size=50)
     pygame.display.flip()
 pygame.quit()
