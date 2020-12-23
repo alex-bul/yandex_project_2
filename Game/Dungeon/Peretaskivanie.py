873d279b577215202d02b11f45f80c7f2ff5df0e
DeDUnitY <gtanegta4@gmail.com>
2020-12-23 21:15:34 +0200

    3

--- Game/Dungeon/Peretaskivanie.py
+++ Game/Dungeon/Peretaskivanie.py
@@ -7,10 +7,12 @@ size = width, height = 1000, 1000
 pygame.display.set_caption('Перетаскивание')
 screen = pygame.display.set_mode(size)
 screen.fill((255, 255, 255))
-fps = 120
+fps = 600
 step = 80
 running = True
 x = 80
+y = 114
+my_name = 0
 
 
 def load_image(name, color_key=None):
@@ -31,58 +33,77 @@ def load_image(name, color_key=None):
 
 
 class Character(pygame.sprite.Sprite):
-    def __init__(self, *group, x, y, name):
+    def __init__(self, *group, x, y, name=f'my_{my_name}', sprite_name):
         pygame.sprite.Sprite.__init__(self)
         super().__init__(*group)
-        self.image = load_image(f'{name}.png')
+        self.image = load_image(f'{sprite_name}.png')
         self.rect = self.image.get_rect()
-        self.rect[0] = x
-        self.rect[1] = y
-        self.def_rect = self.rect.copy()
-        self.click = False
         self.name = name
-        self.health = 0
-        self.armor = 0
-        self.damage = 0
+        self.rect.bottomleft = x, y
+        self.def_rect = self.rect.bottomleft
+        self.click = False
+        self.sprite_name = sprite_name
+        self.health = 1
+        self.armor = 1
+        self.damage = 1
+        self.flag = False
 
     def sprite(self, state=None):
         if state == 1:
-            self.image = load_image(f'{self.name}.png')
+            self.image = load_image(f'{self.sprite_name}.png')
         elif state == 2:
-            self.image = load_image(f'{self.name}_attack.png')
+            self.image = load_image(f'{self.sprite_name}_attack.png')
         else:
-            self.image = load_image(f'{self.name}_death.png')
+            self.image = load_image(f'{self.sprite_name}_death.png')
 
     def update(self, *args):
-        if 'pos' in str(args[0]):
-            if args[0].type == pygame.MOUSEBUTTONUP and args[0].button == 1:
+        if 'pos' in str(args[0]) and not self.health <= 0:
+            if args[0].type == pygame.MOUSEBUTTONUP and args[0].button == 1 and self.rect.collidepoint(
+                    args[0].pos) and self.click:
                 self.click = False
-                # self.rect = self.def_rect.copy()
+                for i in args[1]:
+                    if self.rect.colliderect(i.rect) and i.name != self.name and not i.health <= 0 and not self.flag:
+                        self.flag = True
+                        print(f'{i.health} - {self.damage - (i.armor / 2)}')
+                        if (self.damage - (i.armor / 2)) > 0:
+                            i.health -= self.damage - (i.armor / 2)
+
+                self.rect.bottomleft = self.def_rect
 
             if (args and args[0].type == pygame.MOUSEBUTTONDOWN and self.rect.collidepoint(args[0].pos)) or self.click:
                 mous_x, mous_y = args[0].pos
                 self.sprite(state=2)
                 self.click = True
+                self.flag = False
                 self.rect.center = (mous_x, mous_y)
+
 
             else:
                 self.sprite(state=1)
 
+        if self.health <= 0:
+            self.sprite()
+
 
 class Assassin(Character):
     def __init__(self, x, y):
-        Character.__init__(self, x=x, y=y, name='Assassin')
+        Character.__init__(self, x=x, y=y, name=f'my_{my_name}', sprite_name='Assassin')
+        self.health = 100
+        self.damage = 20
 
 
 class Berserk(Character):
     def __init__(self, x, y):
-        Character.__init__(self, x=x, y=y, name="berserk")
+        Character.__init__(self, x=x, y=y, name=f'my_{my_name}', sprite_name="berserk")
+        self.health = 80
+        self.damage = 40
+        self.armor = 50
 
 
 clock = pygame.time.Clock()
 all_sprites = pygame.sprite.Group()
 
-heros = [Assassin(x=0, y=0)]
+heros = [Assassin(x=0, y=y)]
 
 # for i in heros:
 #     print(list(i.rect.center)[0] + x, list(i.rect.center)[1])
@@ -106,13 +127,14 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-        if key[pygame.K_RIGHT] and (key[pygame.K_1] or key[pygame.K_2]) and not change and len(heros) < 50:
+        if key[pygame.K_RIGHT] and (key[pygame.K_1] or key[pygame.K_2]) and not change and len(heros) < 4:
             all_sprites.remove(heros)
+            my_name += 1
             if key[pygame.K_1]:
-                a = Assassin(x=random.randint(0, 900), y=random.randint(0, 900))
+                a = Assassin(x=x, y=y)
 
             if key[pygame.K_2]:
-                a = Berserk(x=random.randint(0, 900), y=random.randint(0, 900))
+                a = Berserk(x=x, y=y)
 
             x += step
             heros.append(a)
@@ -120,6 +142,7 @@ while running:
 
         if key[pygame.K_LEFT] and len(heros) > 0 and not change:
             all_sprites.remove(heros)
+            my_name -= 1
             heros = heros[:-1]
             x -= step
             all_sprites.add(heros)
@@ -127,15 +150,10 @@ while running:
         if key[pygame.K_SPACE]:
             for i in heros:
                 i.sprite(2)
-                i.rect.center = (random.randint(0, 900), random.randint(0, 900))
-
-        if key[pygame.K_a]:
-            for i in heros:
-                i.rect.center = (500, 500)
 
     screen.fill((255, 255, 255))
     all_sprites.draw(screen)
-    all_sprites.update(event)
+    all_sprites.update(event, heros)
     clock.tick(fps)
     pygame.display.flip()
 pygame.quit()
